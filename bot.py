143a407d76818bc0f45e550a8c8250e4c958fc9d
Иващенко Дмитрий Александрович <ivashchenko.d.a@mac205-02.ouiit.local>
2025-03-20 09:52:10 +0300

    Initial commit: Создан echo bot с использованием Aiogram

---
+++ bot.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, types, Router, filters, F
+import asyncio
+API_TOKEN = "8196643177:AAHCeckFDaBaQZPog1w0llIFKmm8mKCYIq4"
+
+bot = Bot(token = "8196643177:AAHCeckFDaBaQZPog1w0llIFKmm8mKCYIq4")
+dp = Dispatcher()
+start_router = Router()
+
+@start_router.message(filters.CommandStart())
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! я echo bot. Напиши мне что-нибудь, и я повторю.")
+
+@start_router.message(F.text)
+async def echo(message : types.Message):
+    await message.answer(message.text)
+
+async def main():
+    dp.include_router(start_router)
+    await bot.delete_webhook(drop_pending_updates = True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
