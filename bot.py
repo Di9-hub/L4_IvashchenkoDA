af58cb288454cbb16cadb453a0afc2944414e098
Иващенко Дмитрий Александрович <ivashchenko.d.a@mac205-02.ouiit.local>
2025-03-24 12:50:13 +0300

    Initial commit: создан echo bot с использованием Aiogram

--- bot.py
+++ bot.py
@@ -1,18 +1,31 @@
-from aiogram import Bot, Dispatcher, types, Router, filters, F
+from aiogram import Bot, Dispatcher, types,Router,filters, F
 import asyncio
-API_TOKEN = "8196643177:AAHCeckFDaBaQZPog1w0llIFKmm8mKCYIq4"
+from config import API_TOKEN
 
-bot = Bot(token = "8196643177:AAHCeckFDaBaQZPog1w0llIFKmm8mKCYIq4")
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+
 start_router = Router()
 
 @start_router.message(filters.CommandStart())
 async def send_welcome(message: types.Message):
-    await message.reply("Привет! я echo bot. Напиши мне что-нибудь, и я повторю.")
+    await message.reply("Привет! Я бот, который повторяет за тобой.")
+
+@start_router.message(filters.Command("help"))
+async def command_help_handler(message: types.Message)-> None:
+    help_text = """
+        Доступные команды:
+        /start - Начать диалог с ботом
+        /help - Получить список доступных команд
+        (Все остальные сообщения) - Бот повторяет ваше сообщения
+        """
+    await message.answer(help_text)
+
 
 @start_router.message(F.text)
-async def echo(message : types.Message):
-    await message.answer(message.text)
+async def echo(message: types.Message):
+    user_message = message.text
+    await message.answer(f"Вы сказали: {user_message}")
 
 async def main():
     dp.include_router(start_router)
